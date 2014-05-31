f85ea867babff16a39e0bd2a37505e5e08e8b030
kahnjw <jarrod@yola.com>
2014-05-31 00:02:54 -0700

    On second thought, it is smarter not to try to help bad APIs
    Assuming the methods the server uses to create, update and delete object can result in nasty idempotency issues

--- src/http-methods.js
+++ src/http-methods.js
@@ -28,22 +28,6 @@ var methodHelper = function(method) {
 
     options = $.extend({}, this.ajaxOptions, options);
 
-    if(method === 'POST' || method === 'PUT') {
-
-      /* If the user specified data to send in the call to put/post use that.
-       * Otherwise use the data stored on the endpoint's data property
-       */
-      options.data = options.data || this.data;
-
-      /* Store data sent in the ajax call in the endpoints data property.
-       */
-      this.data = options.data;
-    }
-
-    if(method === 'DELETE') {
-      this.data = {};
-    }
-
     qjax.methodFactory(method)(options)
     .then(resolve.bind(this), reject.bind(this));
 
